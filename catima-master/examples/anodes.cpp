9a0b0aa2ae62baf0040e5456979efe05d4785af8
Jordan Cory <cory@frib.msu.edu>
2025-06-06 09:57:33 -0400

    cleaned up anodes

--- catima-master/examples/anodes.cpp
+++ catima-master/examples/anodes.cpp
@@ -13,86 +13,77 @@
 #include "TTree.h"
 #include "TF1.h"
 #include "TString.h"
+#include "runinfo.h"
 using std::cout;
 using std::endl;
+using namespace info;
 //simulation file that creates traces for beam or fusion events, all events convoluted with gaussian 100kev FWHM for music anode resolution 
 
-//to run: ./music_dedx (anode #)
+//to run: ./anodes runnb
 int main(int argc,char **argv){
-//NOTE::: NEED TO ADD IN DIRECTION OF PARTICLE    
-  
+//NOTE::: NEED TO ADD IN DIRECTION OF PARTICLE   
+ 
+    //setting run number and associated variables - see runinfo.cpp for beam, energy, gas, ect.  
     int runnb = 2448;
     if (argc==2){runnb=atoi(argv[1]);} 
-
-    double den = 1.3e-4;
-    if (argc==3){den==0; }//characterize conversion between pressure in torr to densit  
+    runinfo run; 
+    run.setinfo(runnb); 
+  
     TApplication *app = new TApplication("app",&argc,argv);//application call to allow figures to pop up in excecutable 
- 
-//inistilize basic gas properties for helium run  
-    catima::Material he;
-    he.add_element(4,2,1);
-    he.density(1.3e-4); //density in g/cm^3
-    he.thickness(1.625e-4); //thickness in g/cm^2
-    
-    float p150 = 1.3e-4;
-    float p100 = 8.7759e-5;
-//initlize basic gas properties for methane run 
-    catima::Material methane;
-    methane.add_element(1,1,4);
-    methane.add_element(12,6,1);
-    methane.density(p150);
-    methane.thickness(p150*0.25);
+   
+    //defniing gas to lose energy in  
+    catima::Material gas;
+    for (int i=0; i<run.gasel; i++){
+	gas.add_element(run.gasA[i],run.gasZ[i],run.gasS[i]);
+    }
+    gas.density(run.density);
+    //thickness defined later bc it changes 
+
+
 //**********************************
 //*******NOTE: CHANGED THICKNES TO 0.25CM 
 //*********************************
    
-//defining commonly used beams 
-    catima::Projectile S30(30,16); // define projectile, ie 30S
-    catima::Projectile O20(20,8);
-
-  //  cout<<"30S->He\n";
-    cout <<"20O->Methane\n";
-//random variable for gaussian later
-   TRandom3 ran;
-//connecting app to the canvas. When window is closed exe will terminate 
+    catima::Projectile proj(run.A,run.Z);
+ 
+    cout <<Form("A=%i, Z=%i on A=%i, Z=%i\n", run.A,run.Z,run.gasA[0],run.gasZ[0]);
+    //random variable for gaussian later
+    TRandom3 ran;
+    //connecting app to the canvas. When window is closed exe will terminate 
    // TCanvas *c1 = new TCanvas();
     //TRootCanvas *rc = (TRootCanvas *)c1->GetCanvasImp();
     //rc->Connect("CloseWindow()","TApplication",app,"Terminate()");
     
     float x[20]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
-    float Eloss_test;
-    int first=0; 
 
-
-	float E_out[21]={0};
-   	E_out[0]=2.95;//should be 2.7 for oxygen but we will scale to 2.055
-        float Eloss[20]={0};
-      //  float E_sig[20];
-       // float ang_sig[20];     
+    float E_out[21]={0};
+    //defining a 1.5 anode thickness to represent snout of gas beam travels through
+    //energy after going through this is the energy found in anode 0 
+    gas.thickness(run.density*1.8);
+    float Estart = run.Ein/run.A;
+    auto init = catima::calculate(proj,gas,Estart); 
+    E_out[0]=Estart - init.Eloss/run.A;
+    gas.thickness(run.density*0.25);//put thickness back to 1/5 of an anode 
+        
+    float Eloss[20]={0};
 //******************************************** 
 //*******begin loop over anodes *****************
 //******************************************* 
-   	 for(int i=0; i<20; i++){
-	     float Etemp[6];
-	     Etemp[6]=0;
- 	     float los_temp[6]={0};
-	     los_temp[0]=E_out[i];
-	     cout << Etemp[6] << endl;
-	     for (int j=0; j<5; j++){
-             auto result = catima::calculate(O20,methane,los_temp[j]); 
- 	     //Etemp[j]=ran.Gaus(result.Eloss,0.1/2.355);
-	     Etemp[j]=result.Eloss;//changed for now bc we are MCing
+    for(int i=0; i<20; i++){
+	float Etemp[6];
+	Etemp[6]=0;
+ 	float los_temp[6]={0};
+	los_temp[0]=E_out[i];
+	//start loop splitting anode into five parts      
+	for (int j=0; j<5; j++){
+             auto result = catima::calculate(proj,gas,los_temp[j]);  
+	     Etemp[j]=result.Eloss;//not conveluted w gaussian yet bc we are comparing to exp data 
 	     Etemp[6]+=Etemp[j];
              los_temp[j+1]=los_temp[j]-(Etemp[j]/20);
-	     }
-       	 //    Eloss[i]=ran.Gaus(result.Eloss,0.1/2.355);
-      	    //Eloss[i]=result.Eloss;
-	    Eloss[i]=Etemp[6];
-            cout << "Eloss " << Eloss[i] << endl; 
-	    E_out[i+1]=E_out[i]-(Eloss[i]/20); 
-           // E_sig[i]=result.sigma_E;
-            //ang_sig[i]=result.sigma_a;
-	} //end loop over anodes
+	}
+	Eloss[i]=Etemp[6];
+        E_out[i+1]=E_out[i]-(Eloss[i]/20); 
+     } //end loop over anodes
 
        
  
@@ -110,39 +101,39 @@ int main(int argc,char **argv){
 //******START LOOP OVER ANODES TO CANVASES
 //*******************************
     for (int j=0;j<20;j++){
-    TH1F *an0 = (TH1F*)expdata->At(j);
-    cout << "got th1F" << endl;
+	TH1F *an0 = (TH1F*)expdata->At(j);
+	cout << "got th1F" << endl;
     
-    TCanvas *c0 = new TCanvas();
-    c0->cd();
-    canvases[j]=c0;
-    Int_t binmax = an0->GetMaximumBin();
-    Double_t x_max = an0->GetBinCenter(binmax);
+	TCanvas *c0 = new TCanvas();
+	c0->cd();
+	canvases[j]=c0;
+	Int_t binmax = an0->GetMaximumBin();
+	Double_t x_max = an0->GetBinCenter(binmax);
    
-    auto fit = new TF1("fit","gaus",x_max-0.15,x_max+0.15);
-    if (j==8 || j==13 || j==17 || j==19){ 
-    an0->Fit(fit,"R");} 
-    else{an0->Fit(fit,"","",x_max-.1,x_max+.1);}
-    Double_t mean = fit->GetParameter(1);
-    cout << mean << " mean " << endl;
-    an0->Draw("same");
+	auto fit = new TF1("fit","gaus",x_max-0.15,x_max+0.15);
+	if (j==8 || j==13 || j==17 || j==19){ 
+    	    an0->Fit(fit,"R");} 
+        else{an0->Fit(fit,"","",x_max-.1,x_max+.1);}
+        Double_t mean = fit->GetParameter(1);
+        cout << mean << " mean " << endl;
+        an0->Draw("same");
 
 
-   TH1F *h = new TH1F("h","h",550,-0.5,5);
-   graphs[j]=h;
-   int fill = fit->GetParameter(0)*11;
-   cout << fill << std::endl;
-   scales[j] = mean/Eloss[j];
-   fwhm[j]=fit->GetParameter(2)*2.355; 
-   for (int i=1; i<fill;i++){
+        TH1F *h = new TH1F("h","h",550,-0.5,5);
+        graphs[j]=h;
+        int fill = fit->GetParameter(0)*11;
+        cout << fill << std::endl;
+        scales[j] = mean/Eloss[j];
+        fwhm[j]=fit->GetParameter(2)*2.355; 
+        for (int i=1; i<fill;i++){
 
-	h->Fill(ran.Gaus(Eloss[j]*scales[j],(0.1/2.355)));
-     }
+           h->Fill(ran.Gaus(Eloss[j]*scales[j],(0.1/2.355)));
+         }
 
-    h->SetLineColor(kRed);
+        h->SetLineColor(kRed);
   
-    h->Draw("same");
-   }//end loop over anodes
+        h->Draw("same");
+    }//end loop over anodes
     TCanvas *c2 = new TCanvas();
     c2->cd();
     TGraph *g1 = new TGraph(20,x,scales);
@@ -153,9 +144,9 @@ int main(int argc,char **argv){
     TGraph *g2 = new TGraph(20,x,fwhm);
     g2->SetTitle("FWHM per anode");
     g2->Draw();
-   cout << "scaling factors per anode : "; 
-   for (int i=0;i<19;i++){cout << scales[i] << ",";}
-   cout << scales[19] << endl;
+    cout << "scaling factors per anode : "; 
+    for (int i=0;i<19;i++){cout << scales[i] << ",";}
+    cout << scales[19] << endl;
     app->Run(kTRUE); //canvas main menu ->File ->QUIT ROOT (I wish there was a better way of doing this but ig its fine
     delete app;     
     input->Close();
